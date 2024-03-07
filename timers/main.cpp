109f16489e00e2796b798b597fba67b164f29a26
Souhayl-s <souhaylsejjari1@gmail.com>
2024-03-06 23:06:41 -0500

    change names

--- timers/main.cpp
+++ timers/main.cpp
@@ -18,34 +18,13 @@ int main(){
 
     Timer minuterie1  = Timer(Unite::SECS) ; 
 
-    int duree = 10 ; // en secondes
 
     minuterie1.demarrerTimer() ; 
-    allumer() ;
 
-    while (minuterie1.obtenirTemps() < duree){
-        if (minuterie1.obtenirTemps() == 5){
-            minuterie1.arreterTimer() ; 
-            // eteindreLed();
-            break ; 
-        }
-    }
-
-    minuterie1.demarrerTimer();
-    eteindreLed() ;        
-
-    while(minuterie1.obtenirTemps() < duree){
-        // eteinte pendant 10 secondes
-    }
-
-    allumer();
-
-    // minuterie1.reprendreTimer() ; // reprendre a la 5e seconde
-    // while (minuterie1.obtenirTemps() < duree){
-    //     // rester eteinte pendant les 5s restantes       
-    // }
-
-    // allumer() ; 
+    _delay_ms(5000) ; 
+    if (minuterie1.obtenirTemps() >= 4){ // est-ce que les interruptions fonctionnenent ?
+        allumer() ;
+    } 
     
 }
 
